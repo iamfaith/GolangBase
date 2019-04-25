c0793693ad183edbbac00209a83b687d5ef52901
xianzixiang <xianzixiang@wps.cn>
2019-04-25 16:17:44 +0800

    add dev

--- buildApp.sh
+++ buildApp.sh
@@ -7,4 +7,4 @@ V=$1
 V=${V:=0.1}
 echo Building App as version: ${V}
 
-docker build -t `echo -n $PROJECT_NAME | awk '{print tolower($0)}'`:$V -f ./docker/Dockerfile --build-arg PROJECT_NAME=$PROJECT_NAME  --build-arg PROJECT_PATH=$PROJECT_PATH .
+docker build -t xianzixiang/`echo -n $PROJECT_NAME | awk '{print tolower($0)}'`:$V -f ./docker/Dockerfile --build-arg PROJECT_NAME=$PROJECT_NAME  --build-arg PROJECT_PATH=$PROJECT_PATH .
