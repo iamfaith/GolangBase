a5b240635c7d43b0e0a70c0e382603f16945376a
xianzixiang <xianzixiang@wps.cn>
2019-04-26 12:07:18 +0800

    add redis-cluster

--- docker/wait_deps.sh
+++ docker/wait_deps.sh
@@ -4,6 +4,13 @@ set -e
 
 cmd="$@"
 
+redis_cluster_ip=`dig +short ${REDIS_NAME}`
+until timeout -t 1 bash -c "</dev/tcp/${redis_cluster_ip}/7000"; do
+    >&2 echo "waiting for redis-cluster to be available"
+    sleep 1
+done
+
+
 mysql_ip=`dig +short ${MYSQL_HOST}`
 until timeout -t 1 bash -c "</dev/tcp/${mysql_ip}/3306"; do
   >&2 echo "waiting for mysql to be available"
