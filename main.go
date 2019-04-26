5c0c56a5436e7432a9e62df73a2647c522d477a5
xianzixiang <xianzixiang@wps.cn>
2019-04-26 15:06:41 +0800

    add build time

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	_ "GolangBase/routers"
+	"GolangBase/util"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
+func CopyBuildTime() {
+	if err := util.CopyDir("/build_time", "/data/build_time"); err != nil {
+		logs.Error(err)
+	}
+}
+
 func main() {
+	go CopyBuildTime()
 	beego.Run()
 }
